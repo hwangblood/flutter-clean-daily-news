646897c94f73c851db37e9e00236f9cdd887804e
hwangblood <hwangblood@gmail.com>
2023-07-09 15:57:36 +0800

    Use Cases

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsApiBaseUrl = 'https://newsapi.org/v2';
+const String newsApiKey = '3d6c17b369d149a7b10aafa5058dabd9';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
--- lib/features/daily_news/data/repositories/article_repository_impl.dart
+++ lib/features/daily_news/data/repositories/article_repository_impl.dart
@@ -1,11 +1,40 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+import 'package:clean_daily_news/core/constants/constants.dart';
 import 'package:clean_daily_news/core/resources/data_state.dart';
+import 'package:clean_daily_news/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:clean_daily_news/features/daily_news/data/models/article.dart';
 import 'package:clean_daily_news/features/daily_news/domain/repositories/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsApiKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailure(
+          DioError(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioErrorType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioError catch (e) {
+      return DataFailure(e);
+    }
   }
 }

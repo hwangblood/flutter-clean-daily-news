8b184e6dd8dc72c34ab7da54a446a90f1d1fda60
hwangblood <hwangblood@gmail.com>
2023-07-09 15:32:31 +0800

    Make Request To API Using Retrofit

--- lib/features/daily_news/data/repositories/article_repository_impl.dart
+++ lib/features/daily_news/data/repositories/article_repository_impl.dart
@@ -1,5 +1,5 @@
-import 'package:clean_daily_news/features/daily_news/data/models/article.dart';
 import 'package:clean_daily_news/core/resources/data_state.dart';
+import 'package:clean_daily_news/features/daily_news/data/models/article.dart';
 import 'package:clean_daily_news/features/daily_news/domain/repositories/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
